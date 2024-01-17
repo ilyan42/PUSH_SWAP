693877ec1fbe00d117de791a2cb411ed8c7d185e
ilyan <ilyanbendib@gmail.com>
2024-01-17 14:16:50 +0100

    v

--- src/main.c
+++ src/main.c
@@ -6,11 +6,11 @@
 /*   By: ilyanbendib <ilyanbendib@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 14:43:50 by ilyanbendib       #+#    #+#             */
-/*   Updated: 2024/01/16 16:41:33 by ilyanbendib      ###   ########.fr       */
+/*   Updated: 2024/01/17 14:16:48 by ilyanbendib      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#include "../include/push_swap.h"
 
 t_list	*ft_lstnew(void *content)
 {
@@ -26,14 +26,5 @@ t_list	*ft_lstnew(void *content)
 
 int main(int ac, char **av)
 {
-	int i = 1;
-	// int nb = ft_atoi(av[i]);
-	char *tab = ft_atoi(av[i]);
-	t_list *str;
-	if (ac == 2)
-	{
-		str = ft_lstnew(&tab);
-		ft_printf("%s", str);
-	}
-	return (0);
+
 }
