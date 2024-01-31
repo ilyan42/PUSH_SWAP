7b11a32b4e6d5df0b4b6e9aad51007d922775e88
ilyan <ilyanbendib@gmail.com>
2024-01-31 14:56:24 +0100

    h

--- src/moves/push_element.c
+++ src/moves/push_element.c
@@ -6,30 +6,35 @@
 /*   By: ilyanbendib <ilyanbendib@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 19:15:35 by ilyanbendib       #+#    #+#             */
-/*   Updated: 2024/01/30 15:02:56 by ilyanbendib      ###   ########.fr       */
+/*   Updated: 2024/01/31 14:56:19 by ilyanbendib      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/push_swap.h"
 
-void pa(t_lst **a, t_lst **b)
+void	push_node(t_stack_node **dest, t_stack_node **src)
 {
-	t_lst *tmp;
-	
-	tmp = (*b)->next;
-	(*b)->next = (*a);
-	(*a) = (*b);
-	(*b) = tmp;
-	ft_printf("pa\n");
+	t_stack_node	*tmp;
+
+	tmp = NULL;
+	if (!(*src))
+		return ;
+	tmp = (*src);
+	(*src) = (*src)->next;
+	tmp->next = (*dest);
+	(*dest) = tmp;
 }
 
-void pb(t_lst **a, t_lst **b)
+void	pa(t_stack_node **a, t_stack_node **b, bool print)
 {
-	t_lst *tmp;
-	
-	tmp = (*a)->next;
-	(*a)->next = (*b);
-	(*b) = (*a);
-	(*a) = tmp;
-	ft_printf("pb\n");
+	push_node(a, b);
+	if (print)
+		write(1, "pa\n", 3);
+}
+
+void	pb(t_stack_node **a, t_stack_node **b, bool print)
+{
+	push_node(b, a);
+	if (print)
+		write(1, "pb\n", 3);
 }
