12e6741e56e843e33a376dc43bb43b52bce41e47
ilyan <ilyanbendib@gmail.com>
2024-02-01 12:38:00 +0100

    c

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ilbendib <ilbendib@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ilyanbendib <ilyanbendib@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 14:43:50 by ilyanbendib       #+#    #+#             */
-/*   Updated: 2024/02/01 11:57:06 by ilbendib         ###   ########.fr       */
+/*   Updated: 2024/02/01 12:36:45 by ilyanbendib      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,12 @@ int main(int ac, char **av)
 
 	a = NULL;
 	b = NULL;
-	if (ac == 1 || !av[1][0])
+	if (ac == 1 || (ac == 2 && !av[1][0]))
 		return (1);
 	else if (ac == 2)
-		av = ft_split(av[1], ' ');
-	ft_init_list_a(&a , av);
+		ft_init_list_a(&a, ft_split(av[1], ' '));
+	else
+		ft_init_list_a(&a, av + 1);
 	if (!list_is_sorted(a))
 	{
 		if (size_lst(a) == 2)
